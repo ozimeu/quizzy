3968dbe3d173054acca339980228139568fa359b
ozim <ozim10@gmail.com>
2016-12-05 21:36:08 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    unauthenticated do
+      root 'home#index', as: :unauthenticated_root
+    end
+
+    authenticated :user do
+      root 'quizzes#new', as: :authenticated_root
+    end
+  end
+  
+  devise_for :users
+
+  resources :quizzes do
+    resources :questions
+  end
+
+  resources :questions do
+    resources :answers
+  end
 end
